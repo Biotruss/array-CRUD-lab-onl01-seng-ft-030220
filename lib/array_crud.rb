437d59698160ed4c19781f2ca841765037a9ffe9
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-06 06:32:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["John", "James", "Jordan"]
+  array = ["John", "James", "Jordan", "Jacob"]
 end
 
 def add_element_to_end_of_array(array, element)
